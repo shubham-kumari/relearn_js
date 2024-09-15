0ad58b2d540fd5b1f4bdf97b9d05299c9b676697
shubham Kumari <77379900+shubham-kumari@users.noreply.github.com>
2024-09-15 02:38:32 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version of js
+
+//alert(3+3) //we are using node js , not browser
+
+console.log(3 + 3)
+console.log("shubham")
+
+
+let name = "shubham" //string
+let age = 18 //number
+let isLoggeedIn = false //boolean
+let state;
+
+// premitive data type
+
+// number = 2 to the power of 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => 
+// symbol => uniqueness
+
+// object
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
