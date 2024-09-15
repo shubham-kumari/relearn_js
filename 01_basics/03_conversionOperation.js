bd24bd7ff40ba22a1a53e186ce6f09dd6ea664d5
shubham Kumari <77379900+shubham-kumari@users.noreply.github.com>
2024-09-15 05:48:51 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = null
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 //conversion
 let valueInNumber = Number(score)
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -14,7 +14,48 @@ console.log(valueInNumber);
 
 let isLoggedIn ="";
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+//********************** Operations *******************/
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //power
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " shubham"
+
+let str3 = str1 + str2;
+console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+let gameCounter = 100;
+console.log(gameCounter++);
+// console.log(++gameCounter);
+
+
+
+
+
+
+
+
+
+
 
 
