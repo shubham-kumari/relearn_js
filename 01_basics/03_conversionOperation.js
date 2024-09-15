2a20c11d33ede075450f5b204ddcf30fb3b04266
shubham Kumari <77379900+shubham-kumari@users.noreply.github.com>
2024-09-15 04:54:53 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = null
+console.log(typeof score);
+console.log(typeof(score));
+
+//conversion
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn ="";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
