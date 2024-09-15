2acff115168604bc1fe9c61f7c511b427a2d7f1b
shubham Kumari <77379900+shubham-kumari@users.noreply.github.com>
2024-09-15 02:16:46 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 1680
+let accountEmail = 'shubham@gmail.com'
+var accountPassword = "12345"
+accountCity = "Patna"
+let accountstate;
+
+// accountId = 2 // not allowed
+
+console.log(accountId);
+
+accountEmail = "sk@gmail.com"
+accountPassword = "789"
+accountCity = "jalandhar"
+
+console.table([accountId, accountEmail, accountCity, accountstate])
+
