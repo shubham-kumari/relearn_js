d6111c36ec7d0afe8ff63911c8be39d2325b0762
shubham Kumari <77379900+shubham-kumari@users.noreply.github.com>
2024-09-17 04:40:33 +0000

    for-loop

---
+++ 05_iterations/01_one.js
@@ -0,0 +1,54 @@
+// for
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        //console.log("5 is best number");
+    }
+    //console.log(element);
+    
+}
+
+// console.log(element);
+
+for (let i = 1; i <= 10; i++) {
+    //console.log(`Outer loop value: ${i}`);
+   for (let j = 1; j <= 10; j++) {
+    //console.log(`Inner loop value ${j} and inner loop ${i}`);
+    //console.log(i + '*' + j + ' = ' + i*j );
+   }
+    
+}
+
+let myArray = ["superman", "batman", "flash"]
+for(let i = 0 ; i < myArray.length ; i++)
+    {
+        // console.log(myArray[i]);
+        
+    } 
+
+
+
+// break and coninue
+// for(let i = 1; i <= 20; i++)
+// {
+//     if(i == 5)
+//     {
+//         console.log("detected 5");
+//         break
+        
+//     }
+//     console.log(`value of i is ${i}`);
+    
+// }
+for(let i = 1; i <= 20; i++)
+{
+    if(i == 5)
+    {
+        console.log("detected 5");
+        continue
+        
+    }
+    console.log(`value of i is ${i}`);
+    
+}
